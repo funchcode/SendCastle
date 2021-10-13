ca6667d3da4edc2dd9cf7320efec1e7052f2edb1
jch <jch@intosoft.kr>
2021-10-13 16:58:06 +0900

    set: 스프링부트 프로젝트 구성

---
+++ src/main/java/com/example/swaggerkyle/SwaggerKyleApplication.java
@@ -0,0 +1,13 @@
+package com.example.swaggerkyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerKyleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerKyleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/swaggerkyle/SwaggerKyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swaggerkyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerKyleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
